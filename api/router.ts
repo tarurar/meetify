51c536daaaef255e08835bcf005f219ab255fac6
tarurar <tarutin_andrew@mail.ru>
2016-06-08 00:58:11 +0400

    added routes for users and workspaces

--- api/router.ts
+++ api/router.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as userModel from '../model/user';
+import * as workspaceModel from '../model/workspace';
 
 export var apiRouter = express.Router();
 
@@ -8,3 +10,39 @@ apiRouter.route('/')
   .get((req, res, next) => {
     res.send({ status: 'OK' });
   });
+
+apiRouter.route('/users')
+  .get((req, res, next) => {
+    userModel.Users.find(null, (err, data) => {
+      if (err) throw err;
+
+      res.send({ users: data });
+    })
+  })
+  .post((req, res, next) => {
+    let item = req.body;
+
+    userModel.Users.create(item, (err, newUser) => {
+      if (err) throw err;
+
+      res.send({ status: 'OK', userId: newUser.id });
+    });
+  });
+
+apiRouter.route('/workspaces')
+  .get((req, res, next) => {
+    workspaceModel.Workspaces.find(null, (err, data) => {
+      if (err) throw err;
+
+      res.send({ workspaces: data });
+    });
+  })
+  .post((req, res, next) => {
+    let item = req.body;
+
+    workspaceModel.Workspaces.create(item, (err, newWorkspace) => {
+      if (err) throw err;
+
+      res.send({ status: 'OK', workspaceId: newWorkspace.id });
+    })
+  })
